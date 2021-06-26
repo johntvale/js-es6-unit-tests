dfb85608677f3b3f7cd8629e6040dd64c6c52b85
johntvale <johntvale@gmail.com>
2021-06-25 22:46:29 -0400

    Requisito 6 resolvido + (regex corrigido).

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,62 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
+    // Teste que o retorno da função é um array. - OK
+    const testArray = () => {
+      const result = Array.isArray(productDetails());
+      if (result === true) {
+        return true;
+      }
+    }
+
+    assert.strictEqual(testArray(), true);
+
+    // Teste que o array retornado pela função contém dois itens dentro. - OK
+    const qtyItems = () => {return productDetails.length};
+    assert.strictEqual(qtyItems(), 2);
+
+    // Teste que os dois itens dentro do array retornado pela função são objetos. - OK
+    const testObjects = () => {
+      const result1 = typeof (productDetails()[0]);
+      const result2 = typeof (productDetails()[1]);
+      
+      if (result1 === 'object' && result2 == 'object') {
+        return true;
+      }
+    }
+
+    assert.strictEqual(testObjects(), true);
+
+    // Teste que os dois objetos são diferentes entre si. - OK
+    const checkValues = () => {
+      const product = productDetails('Alcool Gel', 'Máscara');
+      const name1 = product[0].name;
+      const name2 = product[1].name;
+      const detailProduct1 = product[0].details.productId;
+      const detailProduct2 = product[1].details.productId;
+
+      if (name1 !== name2 && detailProduct1 !== detailProduct2) {
+        return true;
+      }
+    }
+
+    assert.deepStrictEqual(checkValues(), true);
+
     // Teste que os dois productIds terminam com 123.
+    const checkFinalNumbers = () => {
+      const lastNumbers = productDetails('Alcool Gel', 'Máscara');
+      const rule = /[1][2][3]{1}$/;
+      const lastNumberP1 = lastNumbers[0].details.productId.match(rule);
+      const lastNumberP2 = lastNumbers[1].details.productId.match(rule);
+
+      if (lastNumberP1[0] === '123' && lastNumberP2[0] === '123') {
+        return true;
+      }
+    }
+
+    assert.deepStrictEqual(checkFinalNumbers(), true);
+
   });
 });
